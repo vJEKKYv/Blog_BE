ed21c0cd24118a594ddd8857a6d1885656857148
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-08-23 01:13:41 +0900

    default project files upload

---
+++ src/main/java/Blog/Blog_Test/BlogTestApplication.java
@@ -0,0 +1,13 @@
+package Blog.Blog_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Blog/Blog_Test/BlogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Blog.Blog_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
