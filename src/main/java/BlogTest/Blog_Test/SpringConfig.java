c80b40d65213a8782bd2bbe721c6d504a46b65ce
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2024-08-26 22:14:36 +0900

    memberRepository 변경 사항 반영

--- src/main/java/BlogTest/Blog_Test/SpringConfig.java
+++ src/main/java/BlogTest/Blog_Test/SpringConfig.java
@@ -1,7 +1,9 @@
 package BlogTest.Blog_Test;
 
 import BlogTest.Blog_Test.aop.TimeTraceAop;
-import BlogTest.Blog_Test.member.repository.SpringDataJpaMemberRepository;
+import BlogTest.Blog_Test.blog.repository.PostRepository;
+import BlogTest.Blog_Test.blog.service.PostService;
+import BlogTest.Blog_Test.member.repository.MemberRepository;
 import BlogTest.Blog_Test.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,16 +11,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    private final SpringDataJpaMemberRepository springDataJpaMemberRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
     @Autowired
-    public SpringConfig(SpringDataJpaMemberRepository springDataJpaMemberRepository) {
-        this.springDataJpaMemberRepository = springDataJpaMemberRepository;
+    public SpringConfig(MemberRepository memberRepository, PostRepository postRepository) {
+        this.memberRepository = memberRepository;
+        this.postRepository = postRepository;
     }
-
     @Bean
     public MemberService memberService() {
-        return new MemberService(springDataJpaMemberRepository);
+        return new MemberService(memberRepository);
     }
+    @Bean
+    public PostService postService() {
+        return new PostService(postRepository);
+    }
+
     @Bean
     public TimeTraceAop timeTraceAop(){
         return new TimeTraceAop();
--- src/main/java/BlogTest/Blog_Test/member/service/MemberService.java
+++ src/main/java/BlogTest/Blog_Test/member/service/MemberService.java
@@ -1,7 +1,7 @@
 package BlogTest.Blog_Test.member.service;
 
 import BlogTest.Blog_Test.member.domain.Member;
-import BlogTest.Blog_Test.member.repository.SpringDataJpaMemberRepository;
+import BlogTest.Blog_Test.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,22 +11,22 @@ import java.util.Optional;
 @Service
 public class MemberService {
 
-    private final SpringDataJpaMemberRepository springDataJpaMemberRepository;
+    private final MemberRepository memberRepository;
 
-    public MemberService(SpringDataJpaMemberRepository springDataJpaMemberRepository) {
-        this.springDataJpaMemberRepository = springDataJpaMemberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
     /*
      * 회원가입
      */
     public Long join (Member member){
         validateDuplicateMember(member); //중복 회원 검증
-        springDataJpaMemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        springDataJpaMemberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
             .ifPresent(m -> {
                 throw  new IllegalStateException("이미 존재하는 회원입니다.");
             });
@@ -36,32 +36,32 @@ public class MemberService {
      */
     public List<Member> findMembers() {
 
-        return springDataJpaMemberRepository.findAll();
+        return memberRepository.findAll();
     }
     public Optional<Member> findOne(Long memberId){
-        return springDataJpaMemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
     /*
      * 단일 회원 조회
      */
     public String findMember(long id) {
-        Optional<Member> member = springDataJpaMemberRepository.findById(id);
+        Optional<Member> member = memberRepository.findById(id);
         return member.get().getName();
     }
 
     public Optional<Member> findByName(String name) {
-        return springDataJpaMemberRepository.findByName(name);
+        return memberRepository.findByName(name);
     }
 
     public void deleteByName(String name) {
-        Member member = springDataJpaMemberRepository.findByName(name)
+        Member member = memberRepository.findByName(name)
                 .orElseThrow(() -> new NoSuchElementException("No member found with name: " + name));
-        springDataJpaMemberRepository.delete(member);
+        memberRepository.delete(member);
     }
     public Member changeName(String name, String newName) {
-        Member member = springDataJpaMemberRepository.findByName(name)
+        Member member = memberRepository.findByName(name)
                 .orElseThrow(() -> new NoSuchElementException("No member found with name: " + name));
         member.setName(newName);
-        return springDataJpaMemberRepository.save(member);
+        return memberRepository.save(member);
     }
 }
